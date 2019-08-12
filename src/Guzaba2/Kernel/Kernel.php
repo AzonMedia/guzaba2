0ea5b9680e38f1332ac6ebf31f8c1a18bd0ee355
Antonia <antonia@azonmedia.com>
2019-08-12 15:21:54 +0300

    m

--- src/Guzaba2/Kernel/Kernel.php
+++ src/Guzaba2/Kernel/Kernel.php
@@ -467,7 +467,6 @@ class Kernel
         if ($RClass->hasOwnMethod('_initialize_class')) {
             call_user_func([$class_name, '_initialize_class']);
         }
-        https://www.youtube.com/watch?v=5QlOnbe1R_8
     }
 
     /**
