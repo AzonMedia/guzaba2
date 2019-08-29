e4ccfc7098fbafe27cbd9c9a7500ae3367105bbf
Ivaylo Atanasov <ivo@azonmedia.com>
2019-08-29 18:15:27 +0300

    ss

--- src/Guzaba2/Orm/Store/Memory.php
+++ src/Guzaba2/Orm/Store/Memory.php
@@ -113,7 +113,7 @@ class Memory extends Store implements StoreInterface
         //$class = get_class($ActiveRecord);
         //$lookup_index = $ActiveRecord->get_lookup_index();
         //$this->data[$class][$lookup_index] = $this->process_instance();
-        $meta = $this->FallbackStore->update_record($ActiveRecord);
+        $this->FallbackStore->update_record($ActiveRecord);
         //the meta data needs to be updated
 
         $lookup_index = self::form_lookup_index($ActiveRecord->get_primary_index());
