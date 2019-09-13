d60e4017fea871b374a6c8ba50158d5c779a0ad6
Antonia <antonia@azonmedia.com>
2019-09-13 11:46:02 +0300

    replacement classes

--- src/Guzaba2/Swoole/ReplacementClasses/Table.php
+++ src/Guzaba2/Swoole/ReplacementClasses/Table.php
@@ -3,12 +3,13 @@ declare(strict_types=1);
 
 namespace Swoole;
 
-public const TYPE_INT = 1;
-public const TYPE_STRING = 7;
-public const TYPE_FLOAT = 6;
-
 class Table
 {
+
+	public const TYPE_INT = 1;
+	public const TYPE_STRING = 7;
+	public const TYPE_FLOAT = 6;
+	
 	public function __construct(int $max_rows)
 	{
 
