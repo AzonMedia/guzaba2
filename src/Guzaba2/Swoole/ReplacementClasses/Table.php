51f806e41caef5df789d74cb1debf444e91653a1
Antonia <antonia@azonmedia.com>
2019-09-13 08:47:30 +0000

    push from appveyor

--- src/Guzaba2/Swoole/ReplacementClasses/Table.php
+++ src/Guzaba2/Swoole/ReplacementClasses/Table.php
@@ -5,13 +5,11 @@ namespace Swoole;
 
 class Table
 {
-
-	public const TYPE_INT = 1;
-	public const TYPE_STRING = 7;
-	public const TYPE_FLOAT = 6;
-	
-	public function __construct(int $max_rows)
-	{
-
-	}
+    public const TYPE_INT = 1;
+    public const TYPE_STRING = 7;
+    public const TYPE_FLOAT = 6;
+    
+    public function __construct(int $max_rows)
+    {
+    }
 }
