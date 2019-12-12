645ae38e98c65b924bd54412ae2bb7e2bf3d81f3
Peter <peter@azonmedia.com>
2019-12-12 13:15:59 +0200

    m

--- src/Guzaba2/Swoole/Debug/Backends/BasicCommands/OrmStoreInfo.php
+++ src/Guzaba2/Swoole/Debug/Backends/BasicCommands/OrmStoreInfo.php
@@ -3,8 +3,10 @@ declare(strict_types=1);
 
 namespace Guzaba2\Swoole\Debug\Backends\BasicCommands;
 
-use Azonmedia\Debug\Interfaces\CommandInterface;
+use Psr\Log\LogLevel;
 use Guzaba2\Kernel\Kernel;
+use Guzaba2\Translator\Translator as t;
+use Azonmedia\Debug\Interfaces\CommandInterface;
 
 class OrmStoreInfo implements CommandInterface
 {
@@ -18,9 +20,35 @@ class OrmStoreInfo implements CommandInterface
 //
 //    protected const CONFIG_RUNTIME = [];
 
+    protected static $commands = [
+        'show ormstore' => [ 'method' => 'debug_get_data', 'help_str' => 'Dumps OrmStore' ],
+        'get ormstore hits' => [ 'method' => 'get_hits', 'help_str' => 'Shows ormstore hits' ],
+        'get ormstore hits percentage' => [ 'method' => 'get_hits_percentage', 'help_str' => 'Shows hits as part of hits + misses' ],
+        'get ormstore misses' => [ 'method' => 'get_misses', 'help_str' => 'Shows ormstore misses' ],
+        'reset ormstore stats' => [ 'method' => 'reset_stats', 'help_str' => 'Resets ormstore stats - resets hits, resets misses' ],
+        'reset ormstore' => [ 'method' => 'reset_all', 'help_str' => 'Resets ormstore - clears cache, resets hits, resets misses' ],
+/*
+        'enable ormstore' => [ 'ToDo' ],
+        'disable ormstore' => [ 'ToDo' ]
+*/
+    ];
+
     public function handle(string $command, string $current_prompt, ?string &$change_prompt_to = NULL) : ?string
     {
         $ret = NULL;
+        $class_name = self::get_class_name();
+
+        $tok = strtok($command, ' ');
+        if (0 === strcasecmp('help', $tok)) {
+            $help_command = preg_replace("/^(\w+\s)/", "", $command);
+            if ($this->can_handle($help_command) || 0 === strcasecmp($class_name, $help_command)) {
+                $ret = self::help($help_command);
+                return $ret;
+            } else {
+                return NULL;
+            }
+        }
+
         if ($this->can_handle($command)) {
             $ret = 'ORM Store details:'.PHP_EOL;
             $structure = [];
@@ -28,28 +56,77 @@ class OrmStoreInfo implements CommandInterface
             $OrmStore = Kernel::get_service('OrmStore');
             $PrimaryOrmStore = $OrmStore;
             do {
+                Kernel::log(get_class($OrmStore), LogLevel::INFO);
                 $structure[] = get_class($OrmStore);
                 $FallbackStore = $OrmStore->get_fallback_store();
                 $OrmStore = $FallbackStore;
             } while ($FallbackStore);
-            $ret .= implode(' --> ', $structure).PHP_EOL;
-            $ret .= print_r($PrimaryOrmStore->debug_get_data(), TRUE);
+
+            $command_ret = call_user_func_array([ $PrimaryOrmStore, self::$commands[$command]['method'] ], []);
+            $ret .= implode(' --> ', $structure) . PHP_EOL;
+            switch ($command) {
+                case 'show ormstore' :
+                    $ret .= print_r($command_ret, TRUE);
+                    break;
+                case 'get ormstore hits' :
+                    $ret .= sprintf(t::_('%s (%s): %s'), $class_name, $command, $command_ret);
+                    break;
+                case 'get ormstore hits percentage' :
+                    $ret .= sprintf(t::_('%s (%s): %s%%'), $class_name, $command, $command_ret);
+                    break;
+                case 'get ormstore misses' :
+                    $ret .= sprintf(t::_('%s (%s): %s'), $class_name, $command, $command_ret);
+                    break;
+                case  'reset ormstore stats' :
+                    $ret .= sprintf(t::_('%s (%s): stats are reset.'), $class_name, $command);
+                    break;
+                case  'reset ormstore' :
+                    $ret .= sprintf(t::_('%s (%s): ormstore is reset.'), $class_name, $command);
+                    break;
+                default :
+                    // error
+                    break;
+            }
         }
+
         return $ret;
     }
 
     public function can_handle(string $command) : bool
     {
-        return $command === 'show ormstore';
+        return array_key_exists($command, self::$commands);
     }
 
     public static function handles_commands() : string
     {
-        return 'show ormstore';
+        $ret = sprintf(t::_('%s avilable commands:%s'), self::get_class_name(), PHP_EOL);
+
+        foreach (self::$commands as $k => $v) {
+            $ret = sprintf(t::_('%s%s%s'), $ret, $k, PHP_EOL);
+        }
+
+        return $ret;
     }
 
-    public static function help() : string
+    public static function help(?string $command = null) : string
     {
-        return 'show ormstore - shows details about ORM store(s)';
+        $class_name = self::get_class_name();
+
+        if (null === $command) {
+            return sprintf(t::_('%s - shows details about ORM store(s) - type help %s to see available commands'), $class_name, strtolower($class_name));
+        } else if (0 === strcasecmp($class_name, $command)) {
+            return self::handles_commands();
+        } else {
+            return sprintf(t::_('%s: %s'), $command, self::$commands[$command]['help_str']);
+        }
+    }
+
+    public static function get_class_name()
+    {
+        if (FALSE !== ($pos = strrpos(static::class, "\\"))) {
+            return substr(static::class, $pos + 1);
+        }
+
+        return static::class;
     }
 }
