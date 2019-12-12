92a5ed9fb8ee061b92ca74ffa0a7948fd85593cf
Peter <peter@azonmedia.com>
2019-12-12 18:52:41 +0200

    m

--- src/Guzaba2/Swoole/Debug/Backends/BasicCommands/QueryCacheInfo.php
+++ src/Guzaba2/Swoole/Debug/Backends/BasicCommands/QueryCacheInfo.php
@@ -8,28 +8,22 @@ use Guzaba2\Kernel\Kernel;
 use Guzaba2\Translator\Translator as t;
 use Azonmedia\Debug\Interfaces\CommandInterface;
 
-class OrmStoreInfo extends \Guzaba2\Swoole\Debug\Backends\BasicCommand
+//class QueryCacheInfo implements CommandInterface
+class QueryCacheInfo extends \Guzaba2\Swoole\Debug\Backends\BasicCommand
 {
-
-    //this class does not extend base so no services here
-//    protected const CONFIG_DEFAULTS = [
-//        'services'      => [
-//            'OrmStore',
-//        ]
-//    ];
-//
-//    protected const CONFIG_RUNTIME = [];
-
+    /*
+     * NOT IMPLEMENTED
+     */
     protected static $commands = [
-        'show ormstore' => [ 'method' => 'debug_get_data', 'help_str' => 'Dumps OrmStore' ],
-        'get ormstore hits' => [ 'method' => 'get_hits', 'help_str' => 'Shows ormstore hits' ],
-        'get ormstore hits percentage' => [ 'method' => 'get_hits_percentage', 'help_str' => 'Shows hits as part of hits + misses' ],
-        'get ormstore misses' => [ 'method' => 'get_misses', 'help_str' => 'Shows ormstore misses' ],
-        'reset ormstore stats' => [ 'method' => 'reset_stats', 'help_str' => 'Resets ormstore stats - resets hits, resets misses' ],
-        'reset ormstore' => [ 'method' => 'reset_all', 'help_str' => 'Resets ormstore - clears cache, resets hits, resets misses' ],
+        'show query cache' => [ 'method' => 'debug_get_data', 'help_str' => 'Dumps Cache' ],
+        'get qcache hits' => [ 'method' => 'get_hits', 'help_str' => 'Shows cache hits' ],
+        'get qcache hits percentage' => [ 'method' => 'get_hits_percentage', 'help_str' => 'Shows hits as part of hits + misses' ],
+        'get qcache misses' => [ 'method' => 'get_misses', 'help_str' => 'Shows cache misses' ],
+        'reset qcache stats' => [ 'method' => 'reset_stats', 'help_str' => 'Resets cache stats - resets hits, resets misses' ],
+        'reset query cache' => [ 'method' => 'reset_all', 'help_str' => 'Resets cache - clears cache, resets hits, resets misses' ],
 /*
-        'enable ormstore' => [ 'ToDo' ],
-        'disable ormstore' => [ 'ToDo' ]
+        'enable cache' => [ 'ToDo' ],
+        'disable cache' => [ 'ToDo' ]
 */
     ];
 
@@ -50,37 +44,37 @@ class OrmStoreInfo extends \Guzaba2\Swoole\Debug\Backends\BasicCommand
         }
 
         if ($this->can_handle($command)) {
-            $ret = 'ORM Store details:'.PHP_EOL;
+            $ret = 'Cache Store details:'.PHP_EOL;
             $structure = [];
             //$OrmStore = self::OrmStore();
-            $OrmStore = Kernel::get_service('OrmStore');
-            $PrimaryOrmStore = $OrmStore;
+            $QueryCacheStore = Kernel::get_service('Cache');
+            $PrimaryQueryCacheStore = $QueryCacheStore;
             do {
-                Kernel::log(get_class($OrmStore), LogLevel::INFO);
-                $structure[] = get_class($OrmStore);
-                $FallbackStore = $OrmStore->get_fallback_store();
-                $OrmStore = $FallbackStore;
+                Kernel::log(get_class($QueryCacheStore), LogLevel::INFO);
+                $structure[] = get_class($QueryCacheStore);
+                $FallbackStore = $QueryCacheStore->get_fallback_store();
+                $QueryCacheStore = $FallbackStore;
             } while ($FallbackStore);
 
-            $command_ret = call_user_func_array([ $PrimaryOrmStore, self::$commands[$command]['method'] ], []);
+            $command_ret = call_user_func_array([ $PrimaryQueryCacheStore, self::$commands[$command]['method'] ], []);
             $ret .= implode(' --> ', $structure) . PHP_EOL;
             switch ($command) {
                 case 'show ormstore' :
                     $ret .= print_r($command_ret, TRUE);
                     break;
-                case 'get ormstore hits' :
+                case 'get qcache hits' :
                     $ret .= sprintf(t::_('%s (%s): %s'), $class_name, $command, $command_ret);
                     break;
-                case 'get ormstore hits percentage' :
+                case 'get qcache hits percentage' :
                     $ret .= sprintf(t::_('%s (%s): %s%%'), $class_name, $command, $command_ret);
                     break;
-                case 'get ormstore misses' :
+                case 'get qcache misses' :
                     $ret .= sprintf(t::_('%s (%s): %s'), $class_name, $command, $command_ret);
                     break;
-                case  'reset ormstore stats' :
+                case  'reset qcache stats' :
                     $ret .= sprintf(t::_('%s (%s): stats are reset.'), $class_name, $command);
                     break;
-                case  'reset ormstore' :
+                case  'reset query cache' :
                     $ret .= sprintf(t::_('%s (%s): ormstore is reset.'), $class_name, $command);
                     break;
                 default :
