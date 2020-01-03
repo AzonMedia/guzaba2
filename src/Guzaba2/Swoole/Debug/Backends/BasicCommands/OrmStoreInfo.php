3b61fc20d60ac7ba631690ca9f24c90300431ab0
Peter <peter@azonmedia.com>
2020-01-03 14:55:24 +0200

    m

--- src/Guzaba2/Swoole/Debug/Backends/BasicCommands/OrmStoreInfo.php
+++ src/Guzaba2/Swoole/Debug/Backends/BasicCommands/OrmStoreInfo.php
@@ -39,7 +39,7 @@ class OrmStoreInfo extends \Guzaba2\Swoole\Debug\Backends\BasicCommand
         $class_name = self::get_class_name();
 
         $tok = strtok($command, ' ');
-        if (0 === strcasecmp('help', $tok)) {
+        if (0 === strcasecmp('help', $tok) || 0 === strcasecmp($class_name, $tok)) {
             $help_command = preg_replace("/^(\w+\s)/", "", $command);
             if ($this->can_handle($help_command) || 0 === strcasecmp($class_name, $help_command)) {
                 $ret = self::help($help_command);
