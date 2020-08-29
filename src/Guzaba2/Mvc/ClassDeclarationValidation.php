9463ddb70285d7bae547b8a973a814a51decdcbd
Vesko Kenashkov <vesko@azonmedia.com>
2020-08-29 21:00:51 +0300

    - added check are all public dynamic methods on the controllers used in the routing map

--- src/Guzaba2/Mvc/ClassDeclarationValidation.php
+++ src/Guzaba2/Mvc/ClassDeclarationValidation.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Guzaba2\Mvc;
 
 use Azonmedia\Reflection\ReflectionClass;
+use Azonmedia\Reflection\ReflectionMethod;
 use Guzaba2\Base\Base;
 use Guzaba2\Base\Exceptions\ClassValidationException;
 use Guzaba2\Kernel\Interfaces\ClassDeclarationValidationInterface;
@@ -17,7 +18,8 @@ abstract class ClassDeclarationValidation extends Base implements ClassDeclarati
 {
     public const VALIDATION_METHODS = [
         'validate_routes',
-        'validate_activerecord_controller_actions',
+        'validate_controller_action_parameters',
+        'validate_controller_actions'
     ];
 
     public const SUPPORTED_ARGUMENT_TYPES = [
@@ -36,6 +38,43 @@ abstract class ClassDeclarationValidation extends Base implements ClassDeclarati
             self::$method_name($ns_prefixes);
         }
         return self::VALIDATION_METHODS;
+    }
+
+    /**
+     * Do not allow public dynamic methods without a route.
+     * @param array $ns_prefixes
+     */
+    public static function validate_controller_actions(array $ns_prefixes): void
+    {
+        $active_record_classes = Controller::get_controller_classes($ns_prefixes);
+        foreach ($active_record_classes as $loaded_class) {
+            if ($loaded_class === ActiveRecordDefaultController::class) {
+                continue;
+            }
+            if (! (new ReflectionClass($loaded_class))->isInstantiable()) {
+                continue;
+            }
+            foreach ( (new ReflectionClass($loaded_class) )->getOwnMethods(ReflectionMethod::IS_PUBLIC) as $RMethod) {
+                if ($RMethod->isStatic()) {
+                    continue;
+                }
+                if ($RMethod->getName() === '_init') {
+                    continue;
+                }
+                $method_found = false;
+                foreach ($loaded_class::get_routes() as $route => $route_data) {
+                    foreach ($route_data as $method => $controller) {
+                        if ($controller[1] === $RMethod->getName()) {
+                            $method_found = true;
+                            break 2;
+                        }
+                    }
+                }
+                if (!$method_found) {
+                    throw new ClassValidationException(sprintf(t::_('The controller %1$s has a public dynamic method %2$s that is not used in the routing table. If a public method is needed please use a static one.'), $loaded_class, $RMethod->getName() ));
+                }
+            }
+        }
     }
 
     /**
@@ -68,21 +107,18 @@ abstract class ClassDeclarationValidation extends Base implements ClassDeclarati
     }
 
     /**
-     * Validates the types of the parameters of the controller actions (the putli
+     * Validates the types of the parameters of the controller actions.
      * @param array $ns_prefixes
      * @throws ClassValidationException
      * @throws \ReflectionException
      */
-    public static function validate_activerecord_controller_actions(array $ns_prefixes): void
+    public static function validate_controller_action_parameters(array $ns_prefixes): void
     {
         $active_record_classes = Controller::get_controller_classes($ns_prefixes);
         foreach ($active_record_classes as $loaded_class) {
-            foreach ((new ReflectionClass($loaded_class))->getMethods(\ReflectionMethod::IS_PUBLIC) as $RMethod) {
+            foreach ((new ReflectionClass($loaded_class))->getOwnMethods(\ReflectionMethod::IS_PUBLIC) as $RMethod) {
                 if ($RMethod->isConstructor()) {
                     continue;
-                }
-                if ($RMethod->getDeclaringClass()->getName() !== $loaded_class) {
-                    continue;//do not validate parent methods
                 }
                 if ($RMethod->isStatic()) {
                     continue;//do not validate the static methods as these are helper methods. The controller actions are only dynamic
